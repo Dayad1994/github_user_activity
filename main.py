a6fea8957677892ed48c515863bd856d0543fa8a
dayan iskhakov <dayan.iskhakov@gmail.com>
2025-07-28 13:35:41 +0300

    created other print_events

--- main.py
+++ main.py
@@ -50,18 +50,55 @@ def main(url: str = 'https://api.github.com/users/{username}/events',
 
 
 def print_event(event, commit_count=0):
+    repo_name = event['repo']['name']
+    
     if commit_count:
         commit_word = "commit" if commit_count == 1 else "commits"
-        repo_name = event['repo']['name']
         print(f"Pushed {commit_count} {commit_word} to {repo_name}")
+    elif event['type'] == 'CommitCommentEvent':
+        print(f'Commented on a commit in {repo_name}')
     elif event['type'] == 'CreateEvent':
         ref_type = event['payload']['ref_type']
-        repo_name = event['repo']['name']
         if ref_type == 'repository':
-            print(f'Created {ref_type} {repo_name}', event['created_at'])
+            print(f'Created {ref_type} {repo_name}')
         else:
             ref = event['payload']['ref']
-            print(f'Created {ref_type} {ref} in {repo_name}', event['created_at'])
+            print(f'Created {ref_type} {ref} in {repo_name}')
+    elif event['type'] == 'DeleteEvent':
+        ref_type = event['payload']['ref_type']
+        ref = event['payload']['ref']
+        print(f'Deleted {ref_type} {ref} in {repo_name}')
+    elif event['type'] == 'ForkEvent':
+        forked_repo_name = event['payload']['forkee']['full_name']        
+        print(f'Forked {repo_name} to {forked_repo_name}')
+    elif event['type'] == 'GollumEvent':
+        pages = event['payload']['pages']        
+        print(f'Updated wiki pages in {repo_name}: {pages}')
+    elif event['type'] == 'IssueCommentEvent':
+        action = event['payload']['action'].title()
+        issue_number = event['payload']['issue']['number']        
+        print(f'{action} a comment on an issue #{issue_number} in {repo_name}')
+    elif event['type'] == 'IssuesEvent':
+        action = event['payload']['action'].title()
+        issue_number = event['payload']['issue']['number']        
+        print(f'{action} an issue #{issue_number} in {repo_name}')
+    elif event['type'] == 'MemberEvent':
+        action = event['payload']['action'].title()
+        member = event['payload']['member']['login']
+        if action == 'added':
+            print(f'{action} {member} as a collaborator in {repo_name}')
+        else:
+            print(f'{action} permissions for {member} in {repo_name}')
+    elif event['type'] == 'PublicEvent':
+        print(f'Made {repo_name} public')
+    elif event['type'] == 'PullRequestEvent':
+        action = event['payload']['action'].title()
+        pr_number = event['payload']['number']
+        print(f'{action} pull request #{pr_number} in {repo_name}')
+    elif event['type'] == 'PullRequestReviewEvent':
+        action = event['payload']['action'].title()
+        pr_number = event['payload']['pull_request']['number']        
+        print(f'{action} a review on pull request #{pr_number} in {repo_name}')
     elif event['type'] == 'WatchEvent':
         print(f'Starred {event['repo']['name']}')
     else:
