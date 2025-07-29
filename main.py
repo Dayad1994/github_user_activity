55b884b68ccea1390c92a0d0f1476784d20bc51e
dayan iskhakov <dayan.iskhakov@gmail.com>
2025-07-29 14:46:49 +0300

    added last conditions to print_event function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 
 def sort_create_events(events):
@@ -55,8 +56,10 @@ def print_event(event, commit_count=0):
     if commit_count:
         commit_word = "commit" if commit_count == 1 else "commits"
         print(f"Pushed {commit_count} {commit_word} to {repo_name}")
+    
     elif event['type'] == 'CommitCommentEvent':
         print(f'Commented on a commit in {repo_name}')
+    
     elif event['type'] == 'CreateEvent':
         ref_type = event['payload']['ref_type']
         if ref_type == 'repository':
@@ -64,24 +67,30 @@ def print_event(event, commit_count=0):
         else:
             ref = event['payload']['ref']
             print(f'Created {ref_type} {ref} in {repo_name}')
+    
     elif event['type'] == 'DeleteEvent':
         ref_type = event['payload']['ref_type']
         ref = event['payload']['ref']
         print(f'Deleted {ref_type} {ref} in {repo_name}')
+    
     elif event['type'] == 'ForkEvent':
         forked_repo_name = event['payload']['forkee']['full_name']        
         print(f'Forked {repo_name} to {forked_repo_name}')
+    
     elif event['type'] == 'GollumEvent':
         pages = event['payload']['pages']        
         print(f'Updated wiki pages in {repo_name}: {pages}')
+    
     elif event['type'] == 'IssueCommentEvent':
         action = event['payload']['action'].title()
         issue_number = event['payload']['issue']['number']        
         print(f'{action} a comment on an issue #{issue_number} in {repo_name}')
+    
     elif event['type'] == 'IssuesEvent':
         action = event['payload']['action'].title()
         issue_number = event['payload']['issue']['number']        
         print(f'{action} an issue #{issue_number} in {repo_name}')
+    
     elif event['type'] == 'MemberEvent':
         action = event['payload']['action'].title()
         member = event['payload']['member']['login']
@@ -89,16 +98,42 @@ def print_event(event, commit_count=0):
             print(f'{action} {member} as a collaborator in {repo_name}')
         else:
             print(f'{action} permissions for {member} in {repo_name}')
+    
     elif event['type'] == 'PublicEvent':
         print(f'Made {repo_name} public')
+    
     elif event['type'] == 'PullRequestEvent':
         action = event['payload']['action'].title()
         pr_number = event['payload']['number']
         print(f'{action} pull request #{pr_number} in {repo_name}')
+    
     elif event['type'] == 'PullRequestReviewEvent':
         action = event['payload']['action'].title()
-        pr_number = event['payload']['pull_request']['number']        
+        pr_number = event['payload']['pull_request']['number']
         print(f'{action} a review on pull request #{pr_number} in {repo_name}')
+    
+    elif event['type'] == 'PullRequestReviewCommentEvent':
+        action = event['payload']['action'].title()
+        if action == 'created':
+            print(f'Commented on a pull request review in {repo_name}')
+        else:
+            print(f'Updated comment on a pull request review in {repo_name}')
+    
+    elif event['type'] == 'PullRequestReviewThreadEvent':
+        action = event['payload']['action'].title()
+        pr_number = event['payload']['pull_request']['number']
+        print(f'Updated a review thread in pull request #{pr_number} in {repo_name}')
+
+    elif event['type'] == 'ReleaseEvent':
+        action = event['payload']['action'].title()
+        release_name = event['payload']['release']['name']
+        print(f'{action} release {release_name} in {repo_name}')
+
+    elif event['type'] == 'SponsorshipEvent':
+        action = event['payload']['action'].title()
+        sponsored_user = event['payload']['sponsorship']['sponsorable']['name']
+        print(f'{action} sponsorship for {sponsored_user}')
+
     elif event['type'] == 'WatchEvent':
         print(f'Starred {event['repo']['name']}')
     else:
@@ -106,4 +141,5 @@ def print_event(event, commit_count=0):
 
 
 if __name__ == "__main__":
-    main()
+    username = sys.argv[1]
+    main(username=username)
